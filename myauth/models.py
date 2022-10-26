b6e864a4dd98fde7f7cd61a4c607be76149e5a57
Asish <sridharnagamani5064@gmail.com>
2022-10-26 16:19:58 +0530

    getting user details,user location added

--- myauth/models.py
+++ myauth/models.py
@@ -1,6 +1,4 @@
 from asyncio.windows_events import NULL
-from email.mime import image
-from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
